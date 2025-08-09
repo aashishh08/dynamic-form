290dd8ae482ed1dbeb827395479811e6b640ef32
aashishh08 <aashishh.kumar08@gmail.com>
2025-08-09 20:03:27 +0530

    fixed deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { CssBaseline, Container } from '@mui/material'
-import App from './App'
-import { store } from './store'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { CssBaseline, Container } from "@mui/material";
+import App from "./App";
+import { store } from "./store";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -18,4 +23,4 @@ createRoot(document.getElementById('root')!).render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
-)
+);
